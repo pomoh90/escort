9149ee83ac885b5cbeb60866fe412f23528c3906
pomoh90 <dacaball14121990@gmail.com>
2025-12-19 18:49:19 +0100

    Update package-lock.json to sync with package.json dependencies

---
+++ app/components/Logo.js
@@ -0,0 +1,32 @@
+// components/Logo.js
+export default function Logo() {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          bottom: '10px',
+          right: '10px',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          padding: '5px',
+          borderRadius: '5px',
+        }}
+      >
+        <a
+          href="https://www.theeroticreview.com/reviews/show.asp?id=401194"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <img
+          src="https://www.theeroticreview.com/site_listing/reviewed_seal.gif"
+          alt="TER"
+          style={{
+            width: '40px',
+            height: '40px',
+            objectFit: 'contain',
+          }}
+        />
+        </a>
+      </div>
+    );
+  }
+  
